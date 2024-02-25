fce3546c8e582314c25875664986ffa3405b3f91
Harsh Khavale <harshkhavale1102@gmail.com>
2024-02-25 15:48:20 +0530

    added tailwind

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,43 @@
 import React from "react";
-import "../styles/navbar.scss";
 import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <p className="logo">Radient Riches LLC</p>
-      <div className="search">
-        <CiSearch className="icon" />
-
-        <input type="text" className="input" />
+    <div className="navbar bg-dark-color h-16 flex justify-center items-center">
+      <p className="logo font-bold">Radient Riches LLC</p>
+      <div className="search bg-light-color w-64 h-10 rounded-md border border-solid flex items-center">
+        <CiSearch className="icon text-dark-color text-lg ml-2" />
+        <input
+          type="text"
+          className="input bg-light-color flex-grow outline-none text-dark-color font-bold pl-2"
+          placeholder="Search..."
+        />
       </div>
       <div className="links">
-        <ul>
+        <ul className="flex gap-8">
           <li>
-            <a href="/">Categories</a>
+            <a
+              href="/"
+              className="text-light-color text-base font-bold hover:text-primary-color"
+            >
+              Categories
+            </a>
           </li>
           <li>
-            <a href="/">Website Builders</a>
+            <a
+              href="/"
+              className="text-light-color text-base font-bold hover:text-primary-color"
+            >
+              Website Builders
+            </a>
           </li>
           <li>
-            <a href="/"> Today's deals</a>
+            <a
+              href="/"
+              className="text-light-color text-base font-bold hover:text-primary-color"
+            >
+              Today's deals
+            </a>
           </li>
         </ul>
       </div>
--- components/WebBuilderCard.tsx
+++ components/WebBuilderCard.tsx
@@ -1,58 +1,86 @@
-// components/WebBuilderCard.js
-
 import React from "react";
-import { GoTrophy } from "react-icons/go";
-import { IoDiamondOutline } from "react-icons/io5";
-import styles from "../styles/webbuildercard.module.scss";
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 
 const WebBuilderCard = ({ data, index }: any) => {
   const { image, tag, title, description, rating, highlight } = data;
-  const renderStars = (starCount: any) => {
+
+  const renderStars = (starCount: number) => {
     const stars = [];
     const fullStars = Math.floor(starCount);
     const hasHalfStar = starCount % 1 !== 0;
 
     for (let i = 0; i < fullStars; i++) {
-      stars.push(<FaStar key={i} className={styles.star} />);
+      stars.push(<FaStar key={i} className="text-gold" />);
     }
 
     if (hasHalfStar) {
-      stars.push(<FaStarHalfAlt key={stars.length} className={styles.star} />);
+      stars.push(<FaStarHalfAlt key={stars.length} className="text-gold" />);
     }
 
     return stars;
   };
+
   return (
-    <div className={styles.card}>
-      <p className={styles.index}>{index + 1}</p>
+    <div className="relative p-8 w-4/5 bg-white text-gray-700 flex items-center gap-8">
+      <p className="absolute top-16 left-[-50px] border-gray-400 border-solid border p-4 rounded-full">
+        {index + 1}
+      </p>
       <div>
-        <img src={image} alt={title} className={styles.image} />
+        <img src={image} alt={title} className="w-36 h-28 rounded-lg" />
         {tag && (
-          <div className={styles.tag}>
+          <div className="flex items-center mb-4 gap-4 bg-darkorange absolute top-[-10px] left-[-10px] rounded-r-lg text-white font-bold p-2 text-base">
             {tag.icon}
             {tag.title}
           </div>
         )}
       </div>
       <div className="info">
-        <p>
+        <p className="text-xl font-bold mb-4">
           <strong>{title} - </strong>
           {description}
         </p>
-        <div className={styles.highlight}>
-          <p className={styles.heading}>Main Highlight</p>
-
+        <div className="text-base p-8">
+          <p className="font-bold pb-4">Main Highlight</p>
           <p>{highlight.description}</p>
+          {highlight.main && (
+            <>
+              <div className="box">
+                {highlight.main.map((item: any, index: any) => {
+                  return (
+                    <div key={index} className="box-item">
+                      {item.index}
+                      {item.quality}
+                    </div>
+                  );
+                })}
+              </div>
+              <p>Why we love it?</p>
+              <div>
+                {highlight.reason.map((item: any, index: any) => {
+                  return (
+                    <div key={index} className="box-item">
+                      {item}
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
         </div>
-        <p className="">show more</p>
+        <p className="text-dodgerblue flex items-center">
+          show more
+          <IoIosArrowDown />
+        </p>
       </div>
 
-      <div className={styles.rating}>
-        <span className={styles.score}>{rating.score}</span>
+      <div className="flex flex-col justify-center items-center gap-8">
+        <span className="text-2xl">{rating.score}</span>
         <span>{rating.tag}</span>
-        <div className={styles.stars}>{renderStars(rating.star)}</div>
-        <button>View</button>
+        <div className="flex items-center">{renderStars(rating.star)}</div>
+        <button className="bg-dodgerblue py-4 px-16 text-white rounded-lg">
+          View
+        </button>
       </div>
     </div>
   );
